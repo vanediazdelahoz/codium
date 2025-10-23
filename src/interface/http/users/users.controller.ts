5df04cb4f84d70bde2af86cbade306e2852226dd
Vanessa Diaz De La Hoz <vanessadelahoz@uninorte.edu.co>
2025-10-22 22:40:55 -0500

    Arreglando el user controller

--- src/interface/http/users/users.controller.ts
+++ src/interface/http/users/users.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from "@nestjs/common"
-import { ApiTags, ApiOperation, ApiBearerAuth } from "@nestjs/swagger"
-import type { GetUserUseCase } from "@core/application/users/usecases/get-user.usecase"
-import type { ListUsersUseCase } from "@core/application/users/usecases/list-users.usecase"
-import { Roles } from "../auth/decorators/roles.decorator"
-import { UserRole } from "@core/domain/users/user.entity"
+import { Controller, Get, Param } from "@nestjs/common"; // Se añade Param
+import { ApiTags, ApiOperation, ApiBearerAuth } from "@nestjs/swagger";
+// CORREGIDO: Se elimina la palabra 'type' para que las importaciones sean reales
+import { GetUserUseCase } from "@core/application/users/usecases/get-user.usecase";
+import { ListUsersUseCase } from "@core/application/users/usecases/list-users.usecase";
+import { Roles } from "../auth/decorators/roles.decorator";
+import { UserRole } from "@core/domain/users/user.entity";
 
 @ApiTags("users")
 @ApiBearerAuth()
@@ -18,12 +19,12 @@ export class UsersController {
   @Roles(UserRole.ADMIN)
   @ApiOperation({ summary: "List all users (Admin only)" })
   async listUsers() {
-    return this.listUsersUseCase.execute()
+    return this.listUsersUseCase.execute();
   }
 
   @Get(":id")
   @ApiOperation({ summary: "Get user by ID" })
-  async getUser(id: string) {
-    return this.getUserUseCase.execute(id)
+  async getUser(@Param("id") id: string) { // Se añade @Param("id") para capturar el valor
+    return this.getUserUseCase.execute(id);
   }
 }
