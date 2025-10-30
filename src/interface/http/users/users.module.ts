c45bf0b16d9fb2a73467b59c3c262775e09b2dc1
Oskle <osklevasquez@gmail.com>
2025-10-29 23:00:43 -0500

    Correciones 2

--- src/interface/http/users/users.module.ts
+++ src/interface/http/users/users.module.ts
@@ -1,22 +1,35 @@
+// src/interface/http/users/users.module.ts
+
 import { Module } from "@nestjs/common";
 import { UsersController } from "./users.controller";
+
+// Casos de Uso
+import { CreateUserUseCase } from "@core/application/users/usecases/create-user.usecase";
 import { GetUserUseCase } from "@core/application/users/usecases/get-user.usecase";
 import { ListUsersUseCase } from "@core/application/users/usecases/list-users.usecase";
+
+// Dependencias
 import { USER_REPOSITORY } from "@core/domain/users/user.repository.port";
 import { UserPrismaRepository } from "@infrastructure/database/prisma/user-prisma.repository";
 import { PrismaService } from "@infrastructure/database/prisma.service";
+import { BcryptService } from "@infrastructure/security/bcrypt.service"; // <-- AÑADIDO
 
 @Module({
   controllers: [UsersController],
   providers: [
+    // Casos de Uso
+    CreateUserUseCase, // <-- AÑADIDO
     GetUserUseCase,
     ListUsersUseCase,
+
+    // Dependencias
     PrismaService,
+    BcryptService, // <-- AÑADIDO: CreateUserUseCase lo necesita
     {
       provide: USER_REPOSITORY,
       useClass: UserPrismaRepository,
     },
   ],
-  exports: [USER_REPOSITORY], // Exportamos para que otros módulos puedan usarlo
+  exports: [USER_REPOSITORY],
 })
 export class UsersModule {}
